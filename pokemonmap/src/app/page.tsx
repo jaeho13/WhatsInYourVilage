0b5d335588fc9d61fd16f56c97bd8f426a0b5246
jaehooo13 <jaehooo13@naver.com>
2025-05-31 23:19:15 +0900

    시작 페이지 디자인 변경

--- pokemonmap/src/app/page.tsx
+++ pokemonmap/src/app/page.tsx
@@ -5,16 +5,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className={style.background}>
-        <Image width={600} height={600} src="/main.png" alt="test" />
+      <div className={style.container}>
+        <div className={style.mainImg}>
+          <Image
+            src="/main.png"
+            alt="test"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
 
-        <Link href={"/map"}>
-          <button>시작하기</button>
-        </Link>
-
-        {/* <Link href={"/test"}>
-          <button>테스트용</button>
-        </Link> */}
+        <div className={style.bind}>
+          <Link href={"/map"}>
+            <button className={style.btn}>시작하기</button>
+          </Link>
+        </div>
       </div>
     </>
   );
