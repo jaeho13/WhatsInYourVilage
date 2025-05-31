532262c2bafc3b0021a3672907feafaf5bc4e142
jaehooo13 <jaehooo13@naver.com>
2025-05-31 23:19:32 +0900

    map 경로일 경우에만 버튼 보이도록 변경

--- pokemonmap/src/app/layout.tsx
+++ pokemonmap/src/app/layout.tsx
@@ -1,22 +1,28 @@
+"use client";
+
 import Link from "next/link";
 import "./globals.css";
 import style from "./layout.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body>
         <div className={style.container}>
-          <header>
+          <header className={style.header}>
             <Link href={"/"}>
               <Image src="/logo.png" width={60} height={60} alt="로고" />
             </Link>
             <div>우리 동네 포켓몬 찾기</div>
+
+            {pathname === "/map" && <button>지도용 버튼</button>}
           </header>
           <main>{children}</main>
         </div>
