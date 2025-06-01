9b8b1d67746bfe2e41b34af2496f4655e3c815a9
jaehooo13 <jaehooo13@naver.com>
2025-06-01 16:39:22 +0900

    셀렉트 박스 옵션 값 구현

--- pokemonmap/src/app/layout.tsx
+++ pokemonmap/src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import style from "./layout.module.css";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
@@ -12,17 +13,59 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+
+  const [defaultValue, setDefaultValue] = useState("");
+
   return (
     <html lang="en">
       <body>
         <div className={style.container}>
           <header className={style.header}>
-            <Link href={"/"}>
-              <Image src="/logo.png" width={60} height={60} alt="로고" />
-            </Link>
-            <div>우리 동네 포켓몬 찾기</div>
+            <div className={style.headerText}>
+              <Link href={"/"}>
+                <Image src="/logo.png" width={60} height={60} alt="로고" />
+              </Link>
+              <div>우리 동네 포켓몬 찾기</div>
+            </div>
 
-            {pathname === "/map" && <button>지도용 버튼</button>}
+            {pathname === "/map" && (
+              <div className={style.headerSelect}>
+                <select
+                  className={style.selectOption}
+                  value={defaultValue}
+                  onChange={(e) => setDefaultValue(e.target.value)}
+                >
+                  <option value="" disabled>
+                    동네 선택하기
+                  </option>
+                  <option value="Gangnam ">강남구</option>
+                  <option value="Gangdong ">강동구</option>
+                  <option value="Gangbuk ">강북구</option>
+                  <option value="Gangseo ">강서구</option>
+                  <option value="Gwanak ">관악구</option>
+                  <option value="Gwangjin ">광진구</option>
+                  <option value="Guro ">구로구</option>
+                  <option value="Geumcheon ">금천구</option>
+                  <option value="Nowon ">노원구</option>
+                  <option value="Dobong ">도봉구</option>
+                  <option value="Dongdaemun ">동대문구</option>
+                  <option value="Dongjak ">동작구</option>
+                  <option value="Mapo ">마포구</option>
+                  <option value="Seodaemun ">서대문구</option>
+                  <option value="Seocho ">서초구</option>
+                  <option value="Seongdong ">성동구</option>
+                  <option value="Seongbuk ">성북구</option>
+                  <option value="Songpa ">송파구</option>
+                  <option value="Yangcheon ">양천구</option>
+                  <option value="Yeongdeungpo ">영등포구</option>
+                  <option value="Yongsan ">용산구</option>
+                  <option value="Eunpyeong ">은평구</option>
+                  <option value="Jongno ">종로구</option>
+                  <option value="Jung ">중구</option>
+                  <option value="Jungnang ">중랑구</option>
+                </select>
+              </div>
+            )}
           </header>
           <main>{children}</main>
         </div>
