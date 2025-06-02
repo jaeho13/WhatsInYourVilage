bbe3239006e4594ea6803da0c97711d73b62046f
jaehooo13 <jaehooo13@naver.com>
2025-06-02 14:13:12 +0900

    타입 분리 / fetch 이미지 호출

--- pokemonmap/src/app/map/[id]/page.tsx
+++ pokemonmap/src/app/map/[id]/page.tsx
@@ -1,51 +1,49 @@
-"use client";
-
-import { useParams } from "next/navigation";
+import Image from "next/image";
 import style from "./page.module.css";
+import { localeMap } from "@/types";
 
-const localeMap: { [key: string]: string } = {
-  Gangnam: "강남구",
-  Gangdong: "강동구",
-  Gangbuk: "강북구",
-  Gangseo: "강서구",
-  Gwanak: "관악구",
-  Gwangjin: "광진구",
-  Guro: "구로구",
-  Geumcheon: "금천구",
-  Nowon: "노원구",
-  Dobong: "도봉구",
-  Dongdaemun: "동대문구",
-  Dongjak: "동작구",
-  Mapo: "마포구",
-  Seodaemun: "서대문구",
-  Seocho: "서초구",
-  Seongdong: "성동구",
-  Seongbuk: "성북구",
-  Songpa: "송파구",
-  Yangcheon: "양천구",
-  Yeongdeungpo: "영등포구",
-  Yongsan: "용산구",
-  Eunpyeong: "은평구",
-  Jongno: "종로구",
-  Jung: "중구",
-  Jungnang: "중랑구",
-};
-
-export default function Page() {
-  const { id } = useParams();
-  const localeKey = id as string;
+export default async function Page({ params }: { params: { id: string } }) {
+  const localeKey = params.id;
 
   const localeName = localeMap[localeKey] ?? "알 수 없는 지역";
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/3`);
+
+  if (!res.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const pokemon = await res.json();
 
   return (
     <div className={style.container}>
       <div className={style.localeText}>{localeName}에 있을 수 있는 포켓몬</div>
       <div className={style.boxBind}>
-        <div className={style.infoBox}>테스트</div>
-        <div className={style.infoBox}>테스트</div>
-        <div className={style.infoBox}>테스트</div>
-        <div className={style.infoBox}>테스트</div>
-        <div className={style.infoBox}>테스트</div>
+        <div className={style.infoBox}>
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className={style.infoBox}>
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={150}
+            height={150}
+          />
+        </div>
+
+        <div className={style.infoBox}>
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={150}
+            height={150}
+          />
+        </div>
       </div>
       <div className={style.bottom}>
         <div className={style.shareBtn}>공유하기</div>
---
+++ pokemonmap/src/types.ts
@@ -0,0 +1,32 @@
+// constants.ts
+export type LocaleMap = {
+    [key: string]: string;
+  };
+
+export const localeMap: LocaleMap = {
+    Gangnam: "강남구",
+    Gangdong: "강동구",
+    Gangbuk: "강북구",
+    Gangseo: "강서구",
+    Gwanak: "관악구",
+    Gwangjin: "광진구",
+    Guro: "구로구",
+    Geumcheon: "금천구",
+    Nowon: "노원구",
+    Dobong: "도봉구",
+    Dongdaemun: "동대문구",
+    Dongjak: "동작구",
+    Mapo: "마포구",
+    Seodaemun: "서대문구",
+    Seocho: "서초구",
+    Seongdong: "성동구",
+    Seongbuk: "성북구",
+    Songpa: "송파구",
+    Yangcheon: "양천구",
+    Yeongdeungpo: "영등포구",
+    Yongsan: "용산구",
+    Eunpyeong: "은평구",
+    Jongno: "종로구",
+    Jung: "중구",
+    Jungnang: "중랑구",
+  };
