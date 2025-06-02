888be12c0200754d3703ab78653371fda9778f73
jaehooo13 <jaehooo13@naver.com>
2025-06-02 16:47:21 +0900

    지역에 맞는 포켓몬 보이도록 타입 작성

--- pokemonmap/src/app/map/[id]/page.tsx
+++ pokemonmap/src/app/map/[id]/page.tsx
@@ -1,49 +1,45 @@
 import Image from "next/image";
 import style from "./page.module.css";
-import { localeMap } from "@/types";
+import { localeMap, pokemonByLocale } from "@/types";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const localeKey = params.id;
-
   const localeName = localeMap[localeKey] ?? "알 수 없는 지역";
 
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/3`);
+  const pokeIds = pokemonByLocale[localeKey] ?? pokemonByLocale[""];
 
-  if (!res.ok) {
+  let pokemons;
+
+  try {
+    const responses = await Promise.all(
+      pokeIds.map(async (id) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        if (!res.ok) {
+          throw new Error();
+        }
+        return await res.json();
+      })
+    );
+    pokemons = responses;
+  } catch (error) {
+    console.log(error);
     return <div>오류가 발생했습니다...</div>;
   }
-
-  const pokemon = await res.json();
 
   return (
     <div className={style.container}>
       <div className={style.localeText}>{localeName}에 있을 수 있는 포켓몬</div>
       <div className={style.boxBind}>
-        <div className={style.infoBox}>
-          <Image
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            width={150}
-            height={150}
-          />
-        </div>
-        <div className={style.infoBox}>
-          <Image
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            width={150}
-            height={150}
-          />
-        </div>
-
-        <div className={style.infoBox}>
-          <Image
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            width={150}
-            height={150}
-          />
-        </div>
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.id} className={style.infoBox}>
+            <Image
+              src={pokemon.sprites.front_default}
+              alt={pokemon.name}
+              width={150}
+              height={150}
+            />
+          </div>
+        ))}
       </div>
       <div className={style.bottom}>
         <div className={style.shareBtn}>공유하기</div>
