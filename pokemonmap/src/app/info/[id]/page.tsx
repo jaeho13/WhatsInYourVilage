35e523bb4d48ad88ea075bd03583d35026a57f11
jaehooo13 <jaehooo13@naver.com>
2025-06-04 16:17:23 +0900

    포켓몬 정보 화면 틀 구현

---
+++ pokemonmap/src/app/info/[id]/page.tsx
@@ -0,0 +1,12 @@
+import style from "./page.module.css";
+
+export default function Page() {
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style.imageBox}>ㅋㅋㅋ</div>
+        <div className={style.textBox}>ㅋㅋㅋ</div>
+      </div>
+    </>
+  );
+}
